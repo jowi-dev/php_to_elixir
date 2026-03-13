1886cae4fd07e3f108c5a885ff1d2a98b4cb67a0
jowi-dev <joey8williams@gmail.com>
2026-03-13 13:59:56 -0400

    Add parser tests for operators, ternary, and type casts
    Test comparison operators (==, !=, ===, !==), logical operators (&&, ||,
    !, precedence), string concatenation (left-associative chaining),
    ternary/null_coalesce/elvis operators, and type casts (int, float,
    string).

--- test/php_to_elixir/parser_test.exs
+++ test/php_to_elixir/parser_test.exs
@@ -103,8 +103,8 @@ defmodule PhpToElixir.ParserTest do
 
     test "chained array access" do
       assert parse_expr!("<?php $our['a']['b'];") ==
-               {:array_access,
-                {:array_access, {:variable, "our"}, {:string, "a"}}, {:string, "b"}}
+               {:array_access, {:array_access, {:variable, "our"}, {:string, "a"}},
+                {:string, "b"}}
     end
 
     test "integer key access" do
@@ -131,14 +131,12 @@ defmodule PhpToElixir.ParserTest do
 
     test "chained property access" do
       assert parse_expr!("<?php $obj->foo->bar;") ==
-               {:property_access,
-                {:property_access, {:variable, "obj"}, "foo"}, "bar"}
+               {:property_access, {:property_access, {:variable, "obj"}, "foo"}, "bar"}
     end
 
     test "property access then array access" do
       assert parse_expr!("<?php $this->data['key'];") ==
-               {:array_access,
-                {:property_access, {:variable, "this"}, "data"}, {:string, "key"}}
+               {:array_access, {:property_access, {:variable, "this"}, "data"}, {:string, "key"}}
     end
   end
 
@@ -177,14 +175,12 @@ defmodule PhpToElixir.ParserTest do
 
     test "chained method calls" do
       assert parse_expr!("<?php $obj->first()->second();") ==
-               {:method_call,
-                {:method_call, {:variable, "obj"}, "first", []}, "second", []}
+               {:method_call, {:method_call, {:variable, "obj"}, "first", []}, "second", []}
     end
 
     test "method call on array access" do
       assert parse_expr!("<?php $arr['key']->method();") ==
-               {:method_call,
-                {:array_access, {:variable, "arr"}, {:string, "key"}}, "method", []}
+               {:method_call, {:array_access, {:variable, "arr"}, {:string, "key"}}, "method", []}
     end
 
     test "property vs method distinguished by lparen" do
@@ -195,6 +191,127 @@ defmodule PhpToElixir.ParserTest do
       # With parens -> method call
       assert parse_expr!("<?php $obj->name();") ==
                {:method_call, {:variable, "obj"}, "name", []}
+    end
+  end
+
+  describe "Step 7: comparison operators" do
+    test "equals" do
+      assert parse_expr!("<?php $x == 'y';") ==
+               {:binary_op, :==, {:variable, "x"}, {:string, "y"}}
+    end
+
+    test "not equals" do
+      assert parse_expr!("<?php $x != 'y';") ==
+               {:binary_op, :!=, {:variable, "x"}, {:string, "y"}}
+    end
+
+    test "strict equals" do
+      assert parse_expr!("<?php $x === 'y';") ==
+               {:binary_op, :===, {:variable, "x"}, {:string, "y"}}
+    end
+
+    test "strict not equals" do
+      assert parse_expr!("<?php $x !== 'y';") ==
+               {:binary_op, :!==, {:variable, "x"}, {:string, "y"}}
+    end
+  end
+
+  describe "Step 8: logical operators" do
+    test "logical and" do
+      assert parse_expr!("<?php $a && $b;") ==
+               {:binary_op, :&&, {:variable, "a"}, {:variable, "b"}}
+    end
+
+    test "logical or" do
+      assert parse_expr!("<?php $a || $b;") ==
+               {:binary_op, :||, {:variable, "a"}, {:variable, "b"}}
+    end
+
+    test "logical not" do
+      assert parse_expr!("<?php !$a;") ==
+               {:unary_op, :!, {:variable, "a"}}
+    end
+
+    test "&& binds tighter than ||" do
+      # $a || $b && $c should parse as $a || ($b && $c)
+      assert parse_expr!("<?php $a || $b && $c;") ==
+               {:binary_op, :||, {:variable, "a"},
+                {:binary_op, :&&, {:variable, "b"}, {:variable, "c"}}}
+    end
+
+    test "double negation" do
+      assert parse_expr!("<?php !!$a;") ==
+               {:unary_op, :!, {:unary_op, :!, {:variable, "a"}}}
+    end
+  end
+
+  describe "Step 9: string concatenation" do
+    test "simple concatenation" do
+      assert parse_expr!("<?php $a . $b;") ==
+               {:binary_op, :., {:variable, "a"}, {:variable, "b"}}
+    end
+
+    test "chained concatenation is left-associative" do
+      assert parse_expr!("<?php $a . $b . $c;") ==
+               {:binary_op, :., {:binary_op, :., {:variable, "a"}, {:variable, "b"}},
+                {:variable, "c"}}
+    end
+
+    test "concatenation with string literals" do
+      assert parse_expr!("<?php 'hello' . ' ' . 'world';") ==
+               {:binary_op, :., {:binary_op, :., {:string, "hello"}, {:string, " "}},
+                {:string, "world"}}
+    end
+  end
+
+  describe "Step 10: ternary, null coalescing, elvis" do
+    test "ternary operator" do
+      assert parse_expr!("<?php $x ? 'a' : 'b';") ==
+               {:ternary, {:variable, "x"}, {:string, "a"}, {:string, "b"}}
+    end
+
+    test "null coalescing" do
+      assert parse_expr!("<?php $x ?? 'default';") ==
+               {:null_coalesce, {:variable, "x"}, {:string, "default"}}
+    end
+
+    test "elvis operator" do
+      assert parse_expr!("<?php $x ?: 'default';") ==
+               {:elvis, {:variable, "x"}, {:string, "default"}}
+    end
+
+    test "null coalesce is right-associative" do
+      assert parse_expr!("<?php $a ?? $b ?? $c;") ==
+               {:null_coalesce, {:variable, "a"},
+                {:null_coalesce, {:variable, "b"}, {:variable, "c"}}}
+    end
+
+    test "ternary with complex condition" do
+      assert parse_expr!("<?php $x == 'y' ? 'yes' : 'no';") ==
+               {:ternary, {:binary_op, :==, {:variable, "x"}, {:string, "y"}}, {:string, "yes"},
+                {:string, "no"}}
+    end
+  end
+
+  describe "Step 11: type casts" do
+    test "(int) cast" do
+      assert parse_expr!("<?php (int)$x;") ==
+               {:type_cast, :int, {:variable, "x"}}
+    end
+
+    test "(float) cast" do
+      assert parse_expr!("<?php (float)$x;") ==
+               {:type_cast, :float, {:variable, "x"}}
+    end
+
+    test "(string) cast" do
+      assert parse_expr!("<?php (string)$x;") ==
+               {:type_cast, :string, {:variable, "x"}}
+    end
+
+    test "cast with spaces" do
+      assert parse_expr!("<?php ( int )$x;") ==
+               {:type_cast, :int, {:variable, "x"}}
     end
   end
 end
